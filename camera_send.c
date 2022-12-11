6796fa714a03626199b535804ee672372452d51d
seunghyun <jason5102@ajou.ac.kr>
2022-12-11 13:18:49 +0000

    modify camera

--- camera_send.c
+++ camera_send.c
@@ -1,40 +1,75 @@
 #include <stdio.h>
-#include <wiringPi.h>
-#include <softPwm.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <string.h>
+#include <sys/socket.h>
 
-const int pinServoUD = 17;
-const int pinServoLR = 18;
-
-int main(void)
+int main(int argc, char *argv[])
 {
-	wiringPiSetupGpio();
+	int sock;
+	struct sockaddr_in serv_addr;
+	char msg[2];
+	char on[2] = "1";
+	int str_len;
+	int light = 0;
 
-	softPwmCreate(pinServoUD, 0, 200);
-	softPwmCreate(pinServoLR, 0, 200);
-	
+	if(argc !=3){
+		printf("Usage : %s <IP> <port>\n", argv[0]);
+		exit(1);
+	}
+
+	serv_sock = socket(PF_INET, SOCK_STREAM, 0);
+	if(sock == -1){
+		error_handling("socket() error");
+	}
+
+	memset(&serv_addr, 0, sizeof(serv_addr));
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin.addr.s_addr = inet_addr(argv[1]);
+	serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
+	serv_addr.sin_port = htons(atoi(argv[2]));
+
+	if(connect(serv_sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) ==-1){
+		error_handling("connect() error");
+	}
+
+	str_len = read(serv_sock. message, sizeof(message)-1);
+
+	if(str_len == -1){
+		error_handling("read() error");
+	}
+	printf("Message from server: %s \n", message);4
+
+	//for jpg
+	size_t fsize, nsize = 0;
+	size_t fsize2;
+
 	while(1)
 	{
-		softPwmWrite(pinServoUD, 1);
-		delay(500);
-		softPwmWrite(pinServoUD, 10);
-		delay(500);
-		softPwmWrite(pinServoUD, 20);
-		delay(500);
-		softPwmWrite(pinServoUD, 10);
-		delay(500);
+		int count = 0;
 
-		system("raspistill -t 10000 -o video.mp4");
+		system("raspistill -o hey.jpg");
+		usleep(1000);
+		file = fopen("hey.jpg", "rb");
 
-		softPwmWrite(pinServoLR, 5);
-		delay(500);
-		softPwmWrite(pinServoLR, 15);
-		delay(500);
-		softPwmWrite(pinServoLR, 25);
-		delay(500);
-		softPwmWrite(pinServoLR, 15);
-		delay(500);
-
-		system("raspistill -t 10000 -o video.mp4");
+		//파일 포인터 맨 끝으로 이동
+		fseek(file, 0, SEEK_END);
+		//file size측정
+		fsize = ftell(file);
+		
+		//다시 file맨앞으로
+		fseek(file, 0, SEEK_SET);
+		while(nsize != fsize){
+			//read file and insert to buffer
+			int fpsize = fread(buf, 1, 256, file);
+			nsize += fpsize;
+			send(serv_sock, buf, fpsize, 0);
 		}
+		fclose(file);
+		usleep(500);
+	}
 	return 0;
 }
